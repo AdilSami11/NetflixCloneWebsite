64d4b27bdd9a5a7a9379e1ed50d78859935374a1
Adil_Sami <147207812+AdilSami11@users.noreply.github.com>
2026-04-11 18:24:04 +0500

    Making some Chnages in Ui Also Added the Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import ProfilesPage from "./Pages/ProfilesPage";
-
+import MainNetflixPage from "./Pages/MainNetflixPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar />
-      <ProfilesPage />
+      <Routes>
+        <Route path="/" element={<ProfilesPage />} />
+        <Route path="/home" element={<MainNetflixPage />} />
+      </Routes>
     </>
   );
 }
--- src/Pages/ProfilesPage.jsx
+++ src/Pages/ProfilesPage.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import profile1 from "../assets/ProfilePicture_1.png";
+import profile2 from "../assets/ProfilePicture_2.png";
+import profile3 from "../assets/ProfilePicture_3.png";
+import profile4 from "../assets/ProfilePicture_4.png";
 
 const ProfilesPage = () => {
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => {
+    navigate("/home");
+  };
   return (
     <div className="profiles-main-wrapper">
       <div className="container">
@@ -8,33 +18,33 @@ const ProfilesPage = () => {
           <h2>Profiles</h2>
         </div>
         <div className="profile-cards-wrapper">
-          <div className="profile-card">
+          <div className="profile-card" onClick={handleProfileClick}>
             <div className="profile-img-wrap">
-              <img src="/src/assets/ProfilePicture_1.png" alt="profile-1" />
+              <img src={profile1} alt="profile-1" />
             </div>
             <div className="profile-info-wrap">
               <h4>Murad</h4>
             </div>
           </div>
-          <div className="profile-card">
+          <div className="profile-card" onClick={handleProfileClick}>
             <div className="profile-img-wrap">
-              <img src="/src/assets/ProfilePicture_2.png" alt="profile-2" />
+              <img src={profile2} alt="profile-2" />
             </div>
             <div className="profile-info-wrap">
               <h4>AdilSami</h4>
             </div>
           </div>
-          <div className="profile-card">
+          <div className="profile-card" onClick={handleProfileClick}>
             <div className="profile-img-wrap">
-              <img src="/src/assets/ProfilePicture_3.png" alt="profile-3" />
+              <img src={profile3} alt="profile-3" />
             </div>
             <div className="profile-info-wrap">
               <h4>Shoaib Afridi</h4>
             </div>
           </div>
-          <div className="profile-card">
+          <div className="profile-card" onClick={handleProfileClick}>
             <div className="profile-img-wrap">
-              <img src="/src/assets/ProfilePicture_4.png" alt="profile-4" />
+              <img src={profile4} alt="profile-4" />
             </div>
             <div className="profile-info-wrap">
               <h4>Asad Khan</h4>
